f7f6e6dbbd36b1127d29704cda2b35bb3e230304
Mateusz Sych <msych@xplus.pl>
2018-02-23 15:08:34 +0100

    instant zoom

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const rocky = require('rocky')
 var fs = require('fs')
 
 const proxy = rocky({ forwardHost: true })
-var data = {};
+var dataImage = {};
+var dataJs = {};
 
 function readFiles(dirname, onFileContent, onError) {
   fs.readdir(dirname, function(err, filenames) {
@@ -28,11 +29,18 @@ proxy
   .useResponse(function (req, res, next) {
     if (req.url.indexOf(".svg") > -1){
         //console.log(req.url.slice(req.url.lastIndexOf("/")+1))   
-        if (data[req.url.slice(req.url.lastIndexOf("/")+1)]!==undefined){
+        if (dataImage[req.url.slice(req.url.lastIndexOf("/")+1)]!==undefined){
                 res.setHeader('HACK', '!!!')
-                res.body = Buffer.from(data[req.url.slice(req.url.lastIndexOf("/")+1)], 'utf8')
+                res.body = Buffer.from(dataImage[req.url.slice(req.url.lastIndexOf("/")+1)], 'utf8')
                 console.log(req.url.slice(req.url.lastIndexOf("/")+1) + " HACKED!")
         }  
+    }
+    else if (req.url.indexOf(".js") > -1){
+      if (dataJs[req.url.slice(req.url.lastIndexOf("/")+1)]!==undefined){
+        res.setHeader('HACK', '!!!')
+        res.body = Buffer.from(dataJs[req.url.slice(req.url.lastIndexOf("/")+1)], 'utf8')
+        console.log(req.url.slice(req.url.lastIndexOf("/")+1) + " HACKED!")
+} 
     }
     next()
   })
@@ -40,10 +48,15 @@ proxy
 proxy.routeAll()
 
 readFiles('img/', function(filename, content) {
-  data[filename] = content;
-  //console.log(data)
- 
+  dataImage[filename] = content;
+  console.log("IMAGE FILE LOADED")
+}, function(err) {
+  throw err;
+});
 
+readFiles('jshack/', function(filename, content) {
+  dataJs[filename] = content;
+  console.log("JS FILE LOADED")
 }, function(err) {
   throw err;
 });
